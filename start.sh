a3103e01daebf9a28a8b972fd335958a796b9981
Royal Divyansh <86223297+RoyalDivyansh@users.noreply.github.com>
2022-08-28 09:49:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b main https://github.com/RoyalDivyansh/Pushpa-filter-Bot
-cd /PushpaRajBot
+cd /pushparaj
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
-python3 PushpaRajBot.py
+python3 pushparaj.py
